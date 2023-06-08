a5e4908716ac5dde19a2d3c9f4be53f2f5fb76ce
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-08 11:30:50 +0300

    changed db

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,42 +1,45 @@
--- Create Suppliers table
-CREATE TABLE suppliers (
-                           supplier_id SERIAL PRIMARY KEY,
-                           name VARCHAR,
-                           address VARCHAR,
-                           contact_number VARCHAR,
-                           email VARCHAR
+CREATE TABLE "suppliers" (
+                             "supplier_id" SERIAL PRIMARY KEY,
+                             "name" VARCHAR NOT NULL,
+                             "address" VARCHAR NOT NULL,
+                             "contact_number" VARCHAR NOT NULL,
+                             "email" VARCHAR NOT NULL,
+                             "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
--- Create Products table
-CREATE TABLE products (
-                          product_id SERIAL PRIMARY KEY,
-                          name VARCHAR,
-                          description TEXT,
-                          category VARCHAR,
-                          supplier_id INT,
-                          cost DECIMAL(10, 2),
-                          selling_price DECIMAL(10, 2),
-                          quantity INT,
-                          FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id)
+CREATE TABLE "products" (
+                            "product_id" SERIAL PRIMARY KEY,
+                            "name" VARCHAR NOT NULL,
+                            "description" TEXT NOT NULL,
+                            "category" VARCHAR NOT NULL,
+                            "supplier_id" INT NOT NULL,
+                            "cost" bigint NOT NULL,
+                            "selling_price" bigint NOT NULL,
+                            "quantity" INT NOT NULL,
+                            "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
--- Create Customers table
-CREATE TABLE customers (
-                           customer_id SERIAL PRIMARY KEY,
-                           name VARCHAR,
-                           address VARCHAR,
-                           contact_number VARCHAR,
-                           email VARCHAR
+CREATE TABLE "customers" (
+                             "customer_id" SERIAL PRIMARY KEY,
+                             "name" VARCHAR NOT NULL,
+                             "address" VARCHAR,
+                             "contact_number" VARCHAR NOT NULL,
+                             "email" VARCHAR,
+                             "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
--- Create Sales table
-CREATE TABLE sales (
-                       sale_id SERIAL PRIMARY KEY,
-                       product_id INT,
-                       customer_id INT,
-                       quantity INT,
-                       sale_date DATE,
-                       total_price DECIMAL(10, 2),
-                       FOREIGN KEY (product_id) REFERENCES products(product_id),
-                       FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+CREATE TABLE "sales" (
+                         "sale_id" SERIAL PRIMARY KEY,
+                         "product_id" INT NOT NULL,
+                         "customer_id" INT NOT NULL,
+                         "quantity" INT NOT NULL,
+                         "sale_date" DATE NOT NULL,
+                         "total_price" bigint NOT NULL,
+                         "created_at" timestamptz NOT NULL DEFAULT (now())
 );
+
+ALTER TABLE "products" ADD FOREIGN KEY ("supplier_id") REFERENCES "suppliers" ("supplier_id");
+
+ALTER TABLE "sales" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("product_id");
+
+ALTER TABLE "sales" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id");

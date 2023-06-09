7f682feba75fa02beff33ff92d7186a2f7e34834
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-09 16:07:25 +0300

    fix: random generators

---
+++ utils/config.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"github.com/spf13/viper"
+	"time"
+)
+
+// Config stores all configuration of the application
+type Config struct {
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	ServerAddress        string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+}
+
+// LoadConfig reads configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}

a9c8e7bf3001021eeecae03b76dab40b11e7d5cd
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-09 16:11:30 +0300

    add rand seed

--- utils/random.go
+++ utils/random.go
@@ -9,6 +9,10 @@ import (
 
 const alphabet = "abcdefghijklmnopqrstwxyz"
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
 func RandomInt(min, max int) int {
 	return min + rand.Intn(max-min+1)
 }

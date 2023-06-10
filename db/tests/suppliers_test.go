bce672c0f38522c51ef0d8bbbda3921a61500d4f
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-10 14:29:00 +0300

    tests: TestGetSupplier

--- db/tests/suppliers_test.go
+++ db/tests/suppliers_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ngenohkevin/speedsales/utils"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 //func nullStrings(value string) sql.NullString {
@@ -41,4 +42,19 @@ func createRandomSuppliers(t *testing.T) db.Supplier {
 
 func TestCreateSupplier(t *testing.T) {
 	createRandomSuppliers(t)
+}
+
+func TestGetProduct(t *testing.T) {
+	supplier1 := createRandomSuppliers(t)
+
+	supplier2, err := testQueries.GetSupplier(context.Background(), supplier1.SupplierID)
+	require.NoError(t, err)
+	require.NotEmpty(t, supplier2)
+
+	require.Equal(t, supplier1.Name, supplier2.Name)
+	require.Equal(t, supplier1.Address, supplier2.Address)
+	require.Equal(t, supplier1.ContactNumber, supplier2.ContactNumber)
+	require.Equal(t, supplier1.Email, supplier2.Email)
+
+	require.WithinDuration(t, supplier1.CreatedAt, supplier2.CreatedAt, time.Second)
 }

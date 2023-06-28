e8a101ec80e88950d218b3ca575899947af15982
ngenohkevin <ngenohkevin19@gmail.com>
2023-06-28 14:53:38 +0300

    migrate down

--- db/migration/000001_init_schema.down.sql
+++ db/migration/000001_init_schema.down.sql
@@ -1,3 +1,8 @@
+drop table if exists code_translator cascade;
+
+drop table if exists salestrace cascade;
+
+drop table if exists sales_till cascade;
 
 drop table if exists sales cascade;
 
@@ -5,5 +10,20 @@ drop table if exists products cascade;
 
 drop table if exists suppliers cascade;
 
+drop table if exists department cascade;
+
+drop table if exists acquisition_items_trail cascade;
+
+drop table if exists receive_log cascade;
+
+drop table if exists dispatch_log cascade;
+
+drop table if exists order_log cascade;
+
 drop table if exists customers cascade;
 
+drop table if exists vehicle_activity_log cascade;
+
+drop table if exists users cascade;
+
+drop table if exists vehicle cascade;

d97a06469ff0af7aae4877e3ad3ce4dfad99d4a9
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-02 23:08:32 +0300

    code_tranlator test

--- db/tests/code_translator_test.go
+++ db/tests/code_translator_test.go
@@ -1,7 +1,16 @@
 package tests
 
-import "testing"
+import (
+	db "github.com/ngenohkevin/speedsales/db/sqlc"
+	"github.com/ngenohkevin/speedsales/utils"
+	"testing"
+)
 
-func createCodeTranslator(t *testing.T) {
-
+func createCodeTranslator(t *testing.T) db.CodeTranslator {
+	arg := db.CreateCodeTranslatorParams{
+		MasterCode: utils.RandomAnyString(),
+		LinkCode:   utils.RandomAnyString(),
+		PkgQty:     0,
+		Discount:   0,
+	}
 }

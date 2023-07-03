b72ea5fdda36efec17136d8590deaab61b5bb457
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-03 12:52:08 +0300

    tests: CreateDepartment

--- db/tests/department_test.go
+++ db/tests/department_test.go
@@ -1,1 +1,33 @@
 package tests
+
+import (
+	"context"
+	db "github.com/ngenohkevin/speedsales/db/sqlc"
+	"github.com/ngenohkevin/speedsales/utils"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createRandomDepartment(t *testing.T) db.Department {
+
+	arg := db.CreateDepartmentParams{
+		Category:    utils.NullStrings(utils.RandomAnyString()),
+		SubCategory: utils.NullStrings(utils.RandomAnyString()),
+		Description: utils.NullStrings(utils.RandomDesc()),
+	}
+	department, err := testQueries.CreateDepartment(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, department)
+
+	require.Equal(t, arg.Category, department.Category)
+	require.Equal(t, arg.SubCategory, department.SubCategory)
+	require.Equal(t, arg.Description, department.Description)
+
+	require.NotZero(t, department.DepartmentID)
+
+	return department
+}
+
+func TestCreateDepartment(t *testing.T) {
+	createRandomDepartment(t)
+}

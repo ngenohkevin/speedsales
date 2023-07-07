038c538fda942f5a65f4e211f73c14a9e4fcdadf
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-07 11:46:45 +0300

    create products schema

--- db/query/products.sql
+++ db/query/products.sql
@@ -1,1 +1,7 @@
 -- name: CreateProducts :one
+INSERT INTO products (
+    name, description, category, department_id, supplier_id, cost, selling_price, wholesale_price, min_margin, quantity
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10
+) RETURNING *;
+

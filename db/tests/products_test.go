a022515197a63bad43626ff739d1eb17b1f09c9e
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-09 17:06:35 +0300

    Random product test

--- db/tests/products_test.go
+++ db/tests/products_test.go
@@ -2,9 +2,21 @@ package tests
 
 import (
 	db "github.com/ngenohkevin/speedsales/db/sqlc"
+	"github.com/ngenohkevin/speedsales/utils"
 	"testing"
 )
 
 func createRandomProduct(t *testing.T) db.Product {
-	
+	arg := db.CreateProductsParams{
+		Name:           utils.RandomName(),
+		Description:    utils.RandomDesc(),
+		Category:       utils.RandomAnyString(),
+		DepartmentID:   int32(utils.RandomAnyInt()),
+		SupplierID:     utils.RandomAnyInt(),
+		Cost:           utils.RandomAnyInt(),
+		SellingPrice:   utils.RandomAnyInt(),
+		WholesalePrice: utils.RandomAnyInt(),
+		MinMargin:      utils.RandomFloat(),
+		Quantity:       utils.RandomAnyInt(),
+	}
 }

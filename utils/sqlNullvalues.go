28808b663b39eaf01d7e0bcfa1db5d213709056e
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-10 12:18:44 +0300

    change to pgx

--- utils/sqlNullvalues.go
+++ utils/sqlNullvalues.go
@@ -1,33 +1,32 @@
 package utils
 
 import (
-	"database/sql"
-	"encoding/json"
-	"github.com/tabbed/pqtype"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
-func NullStrings(value string) sql.NullString {
-	return sql.NullString{
+func NullStrings(value string) pgtype.Text {
+	return pgtype.Text{
 		String: value,
 		Valid:  true,
 	}
 }
 
-func NullInt64(value int64) sql.NullInt64 {
-	return sql.NullInt64{
+func NullInt64(value int64) pgtype.Int8 {
+	return pgtype.Int8{
 		Int64: value,
 		Valid: true,
 	}
 }
-func NullRawMessage(value json.RawMessage) pqtype.NullRawMessage {
-	return pqtype.NullRawMessage{
-		RawMessage: value,
-		Valid:      true,
-	}
-}
 
-func NullBool(value bool) sql.NullBool {
-	return sql.NullBool{
+//func NullRawMessage(value json.RawMessage) pqtype.NullRawMessage {
+//	return pqtype.NullRawMessage{
+//		RawMessage: value,
+//		Valid:      true,
+//	}
+//}
+
+func NullBool(value bool) pgtype.Bool {
+	return pgtype.Bool{
 		Bool:  value,
 		Valid: true,
 	}

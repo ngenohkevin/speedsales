923c8fcffd2845d380bf6f30860651a6d0a684eb
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-12 11:16:07 +0300

    Migration: change primary key to BIGSERIAL

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "users" (
-                         "user_id" SERIAL PRIMARY KEY,
+                         "user_id" BIGSERIAL PRIMARY KEY,
                          "username" VARCHAR UNIQUE,
                          "branch" VARCHAR,
                          "stk_location" VARCHAR,
@@ -10,53 +10,50 @@ CREATE TABLE "users" (
 );
 
 CREATE TABLE "suppliers" (
-                             "supplier_id" SERIAL PRIMARY KEY,
+                             "supplier_id" BIGSERIAL PRIMARY KEY,
                              "name" VARCHAR NOT NULL,
                              "address" VARCHAR NOT NULL,
                              "contact_number" VARCHAR NOT NULL,
                              "email" VARCHAR NOT NULL,
-                             "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+                             "created_at" timestamptz NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "code_translator" (
-                                   "master_code" VARCHAR,
+                                   "master_code" VARCHAR PRIMARY KEY,
                                    "link_code" VARCHAR UNIQUE NOT NULL,
                                    "pkg_qty" FLOAT NOT NULL DEFAULT 0,
-                                   "discount" FLOAT NOT NULL DEFAULT 0,
-                                   PRIMARY KEY ("master_code")
+                                   "discount" FLOAT NOT NULL DEFAULT 0
 );
 
 CREATE TABLE "department" (
-                              "department_id" SERIAL PRIMARY KEY,
+                              "department_id" BIGSERIAL PRIMARY KEY,
                               "category" VARCHAR,
                               "sub_category" VARCHAR,
                               "description" VARCHAR
 );
 
 CREATE TABLE "products" (
-                            "product_id" SERIAL UNIQUE PRIMARY KEY NOT NULL,
+                            "product_id" BIGSERIAL UNIQUE PRIMARY KEY NOT NULL,
                             "name" VARCHAR NOT NULL,
                             "description" TEXT NOT NULL,
                             "category" VARCHAR NOT NULL,
-                            "department_id" INT NOT NULL,
+                            "department_id" BIGINT NOT NULL,
                             "supplier_id" BIGINT NOT NULL,
                             "cost" BIGINT NOT NULL,
                             "selling_price" BIGINT NOT NULL,
                             "wholesale_price" BIGINT NOT NULL,
                             "min_margin" FLOAT NOT NULL,
                             "quantity" BIGINT NOT NULL,
-                            "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                            FOREIGN KEY ("department_id") REFERENCES "department" ("department_id"),
-                            FOREIGN KEY ("supplier_id") REFERENCES "suppliers" ("supplier_id")
+                            "created_at" timestamptz NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "customers" (
-                             "customer_id" SERIAL PRIMARY KEY,
+                             "customer_id" BIGSERIAL PRIMARY KEY,
                              "name" VARCHAR NOT NULL,
                              "address" VARCHAR,
                              "contact_number" VARCHAR NOT NULL,
                              "email" VARCHAR,
-                             "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW()
+                             "created_at" timestamptz NOT NULL DEFAULT (NOW())
 );
 
 CREATE TABLE "sales_till" (
@@ -64,9 +61,9 @@ CREATE TABLE "sales_till" (
                               "teller" VARCHAR,
                               "supervisor" VARCHAR,
                               "branch" VARCHAR,
-                              "open_time" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              "open_time" timestamptz NOT NULL DEFAULT (NOW()),
                               "open_cash" FLOAT NOT NULL DEFAULT 0,
-                              "close_time" TIMESTAMPTZ,
+                              "close_time" timestamptz,
                               "close_cash" FLOAT,
                               "close_summary" JSONB
 );
@@ -74,7 +71,7 @@ CREATE TABLE "sales_till" (
 CREATE TABLE "sales" (
                          "receipt_num" BIGINT PRIMARY KEY,
                          "till_num" BIGINT,
-                         "txn_time" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                         "txn_time" timestamptz NOT NULL DEFAULT (NOW()),
                          "product_id" BIGINT NOT NULL,
                          "item_name" VARCHAR,
                          "price" FLOAT NOT NULL DEFAULT 0,
@@ -88,59 +85,51 @@ CREATE TABLE "sales" (
                          "serial_code_return" VARCHAR,
                          "served_by" VARCHAR,
                          "approved_by" VARCHAR,
-                         "state" VARCHAR(30) NOT NULL DEFAULT 'active',
-                         FOREIGN KEY ("product_id") REFERENCES "products" ("product_id")
+                         "state" VARCHAR(30) NOT NULL DEFAULT 'active'
 );
 
 CREATE TABLE "salestrace" (
-                              "sale_id" SERIAL PRIMARY KEY,
-                              "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                              "sale_id" BIGSERIAL PRIMARY KEY,
+                              "created_at" timestamptz NOT NULL DEFAULT (NOW()),
                               "receipt_num" BIGINT NOT NULL,
                               "till_num" BIGINT,
                               "smart_card" INT NOT NULL DEFAULT 0,
                               "customer_id" BIGINT NOT NULL,
                               "quantity" BIGINT NOT NULL,
-                              "sale_time" TIMESTAMPTZ NOT NULL,
+                              "sale_time" timestamptz NOT NULL,
                               "total_price" BIGINT NOT NULL,
                               "customer_num" BIGINT,
                               "cash_paid" FLOAT NOT NULL DEFAULT 0,
                               "payment_summary" JSONB,
                               "change" FLOAT NOT NULL DEFAULT 0,
-                              "state" VARCHAR(30),
-                              FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id"),
-                              FOREIGN KEY ("till_num") REFERENCES "sales_till" ("till_num"),
-                              FOREIGN KEY ("receipt_num") REFERENCES "sales" ("receipt_num")
+                              "state" VARCHAR(30)
 );
 
 CREATE TABLE "order_log" (
-                             "order_time" TIMESTAMPTZ,
+                             "order_time" timestamptz,
                              "order_num" BIGINT UNIQUE,
                              "poster" VARCHAR,
                              "approver" VARCHAR,
                              "customer_id" BIGINT,
-                             "retailer" VARCHAR,
-                             FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id")
+                             "retailer" VARCHAR
 );
 
 CREATE TABLE "dispatch_log" (
-                                "dispatch_time" TIMESTAMPTZ,
+                                "dispatch_time" timestamptz,
                                 "dispatch_id" BIGSERIAL PRIMARY KEY,
                                 "poster" VARCHAR,
                                 "approver" VARCHAR,
                                 "order_num" BIGINT,
-                                "vehicle" VARCHAR,
-                                FOREIGN KEY ("order_num") REFERENCES "order_log" ("order_num")
+                                "vehicle" VARCHAR
 );
 
 CREATE TABLE "receive_log" (
-                               "receive_time" TIMESTAMPTZ,
+                               "receive_time" timestamptz,
                                "receive_id" BIGSERIAL PRIMARY KEY,
                                "poster" VARCHAR,
                                "approver" VARCHAR,
                                "dispatch_id" BIGINT,
-                               "order_num" BIGINT,
-                               FOREIGN KEY ("dispatch_id") REFERENCES "dispatch_log" ("dispatch_id"),
-                               FOREIGN KEY ("order_num") REFERENCES "order_log" ("order_num")
+                               "order_num" BIGINT
 );
 
 CREATE TABLE "acquisition_items_trail" (
@@ -155,20 +144,17 @@ CREATE TABLE "acquisition_items_trail" (
                                            "price" BIGINT,
                                            "cost" BIGINT,
                                            "vat_code" VARCHAR,
-                                           "vat" FLOAT,
-                                           FOREIGN KEY ("order_id") REFERENCES "order_log" ("order_num"),
-                                           FOREIGN KEY ("dispatch_id") REFERENCES "dispatch_log" ("dispatch_id"),
-                                           FOREIGN KEY ("receive_id") REFERENCES "receive_log" ("receive_id")
+                                           "vat" FLOAT
 );
 
 CREATE TABLE "vehicle" (
-                           "vehicle_id" SERIAL PRIMARY KEY NOT NULL,
+                           "vehicle_id" BIGSERIAL PRIMARY KEY NOT NULL,
                            "vehicle_name" VARCHAR,
                            "registration_num" VARCHAR NOT NULL,
                            "vin_num" VARCHAR,
                            "manufacture_date" DATE,
                            "mileage" FLOAT,
-                           "last_mileage_read" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                           "last_mileage_read" timestamptz NOT NULL DEFAULT (NOW()),
                            "insurance_expiry" DATE NOT NULL,
                            "checklist" JSONB,
                            "state" VARCHAR
@@ -176,12 +162,40 @@ CREATE TABLE "vehicle" (
 
 CREATE TABLE "vehicle_activity_log" (
                                         "activity_id" BIGSERIAL PRIMARY KEY NOT NULL,
-                                        "activity_time" TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+                                        "activity_time" timestamptz NOT NULL DEFAULT (NOW()),
                                         "done_by" VARCHAR,
                                         "vehicle_id" INT NOT NULL,
                                         "approved_by" VARCHAR,
                                         "checklist" JSONB,
-                                        "notation" VARCHAR,
-                                        FOREIGN KEY ("vehicle_id") REFERENCES "vehicle" ("vehicle_id"),
-                                        FOREIGN KEY ("done_by") REFERENCES "users" ("username")
+                                        "notation" VARCHAR
 );
+
+ALTER TABLE "products" ADD FOREIGN KEY ("department_id") REFERENCES "department" ("department_id");
+
+ALTER TABLE "products" ADD FOREIGN KEY ("supplier_id") REFERENCES "suppliers" ("supplier_id");
+
+ALTER TABLE "sales" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("product_id");
+
+ALTER TABLE "salestrace" ADD FOREIGN KEY ("receipt_num") REFERENCES "sales" ("receipt_num");
+
+ALTER TABLE "salestrace" ADD FOREIGN KEY ("till_num") REFERENCES "sales_till" ("till_num");
+
+ALTER TABLE "salestrace" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id");
+
+ALTER TABLE "order_log" ADD FOREIGN KEY ("customer_id") REFERENCES "customers" ("customer_id");
+
+ALTER TABLE "dispatch_log" ADD FOREIGN KEY ("order_num") REFERENCES "order_log" ("order_num");
+
+ALTER TABLE "receive_log" ADD FOREIGN KEY ("dispatch_id") REFERENCES "dispatch_log" ("dispatch_id");
+
+ALTER TABLE "receive_log" ADD FOREIGN KEY ("order_num") REFERENCES "order_log" ("order_num");
+
+ALTER TABLE "acquisition_items_trail" ADD FOREIGN KEY ("order_id") REFERENCES "order_log" ("order_num");
+
+ALTER TABLE "acquisition_items_trail" ADD FOREIGN KEY ("dispatch_id") REFERENCES "dispatch_log" ("dispatch_id");
+
+ALTER TABLE "acquisition_items_trail" ADD FOREIGN KEY ("receive_id") REFERENCES "receive_log" ("receive_id");
+
+ALTER TABLE "vehicle_activity_log" ADD FOREIGN KEY ("done_by") REFERENCES "users" ("username");
+
+ALTER TABLE "vehicle_activity_log" ADD FOREIGN KEY ("vehicle_id") REFERENCES "vehicle" ("vehicle_id");

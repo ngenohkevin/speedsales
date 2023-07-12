0cc0b65155132cfe49c80bce932b3f12979cabf0
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-12 10:13:00 +0300

    Tests: GetProduct

--- db/tests/products_test.go
+++ db/tests/products_test.go
@@ -25,9 +25,32 @@ func createRandomProduct(t *testing.T) db.Product {
 	require.NoError(t, err)
 	require.NotEmpty(t, product)
 
+	require.NotZero(t, product.ProductID)
+	require.NotZero(t, product.CreatedAt)
+
 	return product
 }
 
 func TestCreateProduct(t *testing.T) {
 	createRandomUsers(t)
+}
+
+func TestGetProduct(t *testing.T) {
+	product1 := createRandomProduct(t)
+
+	product2, err := testQueries.GetProducts(context.Background(), product1.ProductID)
+	require.NoError(t, err)
+	require.NotEmpty(t, product2)
+
+	require.Equal(t, product1.ProductID, product2.ProductID)
+	require.Equal(t, product1.Name, product2.Name)
+	require.Equal(t, product1.Description, product2.Description)
+	require.Equal(t, product1.Category, product2.Category)
+	require.Equal(t, product1.DepartmentID, product2.DepartmentID)
+	require.Equal(t, product1.SupplierID, product2.SupplierID)
+	require.Equal(t, product1.Cost, product2.Cost)
+	require.Equal(t, product1.SellingPrice, product2.SellingPrice)
+	require.Equal(t, product1.WholesalePrice, product2.WholesalePrice)
+	require.Equal(t, product1.MinMargin, product2.MinMargin)
+	require.Equal(t, product1.Quantity, product2.Quantity)
 }

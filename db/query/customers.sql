e954e80c0363922094bb0b2424aaf6f4d4c7ca27
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-13 10:09:14 +0300

    SQLC: get customer schema

--- db/query/customers.sql
+++ db/query/customers.sql
@@ -4,4 +4,11 @@ INSERT INTO customers (
     name, address, contact_number, email
 ) VALUES (
  $1, $2, $3, $4
-)
+) RETURNING *;
+
+-- name: GetCustomers :one
+SELECT * FROM customers
+WHERE customer_id = $1
+ORDER BY name
+LIMIT $2
+OFFSET $3;

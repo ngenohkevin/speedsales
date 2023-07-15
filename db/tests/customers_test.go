785460abd8e69a77af4133074f50c229af323c51
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-15 10:58:31 +0300

    Tests: CreateCustomer

--- db/tests/customers_test.go
+++ db/tests/customers_test.go
@@ -1,1 +1,35 @@
 package tests
+
+import (
+	"context"
+	db "github.com/ngenohkevin/speedsales/db/sqlc"
+	"github.com/ngenohkevin/speedsales/utils"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func createRandomCustomer(t *testing.T) db.Customer {
+	arg := db.CreateCustomerParams{
+		Name:          utils.RandomName(),
+		Address:       utils.NullStrings(utils.RandomAddress()),
+		ContactNumber: utils.RandomContact(),
+		Email:         utils.NullStrings(utils.RandomEmail()),
+	}
+
+	customer, err := testQueries.CreateCustomer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, customer)
+
+	require.Equal(t, arg.Name, customer.Name)
+	require.Equal(t, arg.Address, customer.Address)
+	require.Equal(t, arg.ContactNumber, customer.ContactNumber)
+	require.Equal(t, arg.Email, customer.Email)
+
+	require.NotZero(t, customer.CustomerID)
+
+	return customer
+}
+
+func TestCreateCustomer(t *testing.T) {
+	createRandomCustomer(t)
+}

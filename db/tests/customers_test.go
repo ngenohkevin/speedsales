5e6bbff12d0aaaaa6d2163374d1db1b7190759f5
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-15 11:11:02 +0300

    Tests:  GetCustomer

--- db/tests/customers_test.go
+++ db/tests/customers_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ngenohkevin/speedsales/utils"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func createRandomCustomer(t *testing.T) db.Customer {
@@ -26,10 +27,46 @@ func createRandomCustomer(t *testing.T) db.Customer {
 	require.Equal(t, arg.Email, customer.Email)
 
 	require.NotZero(t, customer.CustomerID)
+	require.NotZero(t, customer.CreatedAt)
 
 	return customer
 }
 
 func TestCreateCustomer(t *testing.T) {
 	createRandomCustomer(t)
+}
+
+func TestGetCustomer(t *testing.T) {
+	customer1 := createRandomCustomer(t)
+
+	customer2, err := testQueries.GetCustomer(context.Background(), customer1.CustomerID)
+	require.NoError(t, err)
+	require.NotEmpty(t, customer2)
+
+	require.Equal(t, customer1.CustomerID, customer2.CustomerID)
+	require.Equal(t, customer1.Name, customer2.Name)
+	require.Equal(t, customer1.Address, customer2.Address)
+	require.Equal(t, customer1.ContactNumber, customer2.ContactNumber)
+	require.Equal(t, customer1.Email, customer2.Email)
+
+	require.WithinDuration(t, customer1.CreatedAt, customer2.CreatedAt, time.Second)
+}
+
+func TestUpdateCustomer(t *testing.T) {
+	customer1 := createRandomCustomer(t)
+
+	arg := db.UpdateCustomerParams{
+		CustomerID:    customer1.CustomerID,
+		Name:          utils.RandomName(),
+		Address:       utils.NullStrings(utils.RandomAddress()),
+		ContactNumber: utils.RandomContact(),
+		Email:         utils.NullStrings(utils.RandomEmail()),
+	}
+
+	customer2, err := testQueries.UpdateCustomer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, customer2)
+
+	require.Equal(t, customer1.CustomerID, customer2.CustomerID)
+
 }

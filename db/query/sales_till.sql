396aa741828c7ab5ebfffcfb9ad696a8899dccee
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-21 10:25:19 +0300

    SQLC: Create Sales_till

--- db/query/sales_till.sql
+++ db/query/sales_till.sql
@@ -1,6 +1,6 @@
 -- name: CreateSales_Till :one
 INSERT INTO sales_till (
-    teller, supervisor, branch, open_time, open_cash, close_time, close_cash, close_summary
+    till_num, teller, supervisor, branch, close_time, close_cash, close_summary
 ) VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8
-) RETURNING *;
+     $1, $2, $3, $4, $5, $6, $7
+)

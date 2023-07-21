8fcc492614542c55a033c449fb50ff1959192edb
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-21 10:27:25 +0300

    SQLC: Get Sales_till

--- db/query/sales_till.sql
+++ db/query/sales_till.sql
@@ -1,6 +1,10 @@
--- name: CreateSales_Till :one
+-- name: CreateSales_till :one
 INSERT INTO sales_till (
     till_num, teller, supervisor, branch, close_time, close_cash, close_summary
 ) VALUES (
      $1, $2, $3, $4, $5, $6, $7
 )
+
+-- name: GetSales_till :one
+SELECT * FROM sales_till
+WHERE till_num = $1 LIMIT 1;

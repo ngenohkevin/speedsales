1e8529881f9f1e44b84f9e311a6585dc42203e9f
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-25 11:03:41 +0300

    Tests: createRandomSales_till

--- db/tests/sales_till_test.go
+++ db/tests/sales_till_test.go
@@ -1,1 +1,21 @@
 package tests
+
+import (
+	"github.com/jackc/pgx/v5/pgtype"
+	db "github.com/ngenohkevin/speedsales/db/sqlc"
+	"github.com/ngenohkevin/speedsales/utils"
+	"testing"
+)
+
+func createRandomSalesTill(t *testing.T) db.SalesTill {
+	arg := db.CreateSales_tillParams{
+		TillNum:      utils.RandomAnyInt(),
+		Teller:       utils.NullStrings(utils.RandomAnyString()),
+		Supervisor:   utils.NullStrings(utils.RandomAnyString()),
+		Branch:       utils.NullStrings(utils.RandomAnyString()),
+		CloseTime:    pgtype.Timestamptz{},
+		CloseCash:    pgtype.Float8{},
+		CloseSummary: nil,
+	}
+
+}

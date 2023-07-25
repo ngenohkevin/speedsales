6471285a698559a88d5f4b52e7cd4609457e5167
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-25 11:23:20 +0300

    Tests: createRandomSales_till

--- db/tests/sales_till_test.go
+++ db/tests/sales_till_test.go
@@ -1,8 +1,10 @@
 package tests
 
 import (
+	"context"
 	db "github.com/ngenohkevin/speedsales/db/sqlc"
 	"github.com/ngenohkevin/speedsales/utils"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
@@ -17,5 +19,9 @@ func createRandomSalesTill(t *testing.T) db.SalesTill {
 		CloseCash:    utils.NullFloat64(utils.RandomFloat()),
 		CloseSummary: utils.RandomJSON(4),
 	}
+	salesTill, err := testQueries.CreateSales_till(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, salesTill)
 
+	return salesTill
 }

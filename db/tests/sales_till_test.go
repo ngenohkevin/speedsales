b853f3855fef69bf5078445d8103817329c7a7d4
ngenohkevin <ngenohkevin19@gmail.com>
2023-07-27 11:33:36 +0300

    fixed closeTime value

--- db/tests/sales_till_test.go
+++ db/tests/sales_till_test.go
@@ -7,15 +7,16 @@ import (
 	"github.com/ngenohkevin/speedsales/utils"
 	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func createRandomSalesTill(t *testing.T) db.SalesTill {
 	arg := db.CreateSales_tillParams{
-		TillNum:    utils.RandomAnyInt(),
-		Teller:     utils.NullStrings(utils.RandomAnyString()),
-		Supervisor: utils.NullStrings(utils.RandomAnyString()),
-		Branch:     utils.NullStrings(utils.RandomAnyString()),
-		//CloseTime:    utils.NullTimeStamp(time.Now()),
+		TillNum:      utils.RandomAnyInt(),
+		Teller:       utils.NullStrings(utils.RandomAnyString()),
+		Supervisor:   utils.NullStrings(utils.RandomAnyString()),
+		Branch:       utils.NullStrings(utils.RandomAnyString()),
+		CloseTime:    utils.NullTimeStamp(time.Date(2023, time.July, 27, 11, 18, 10, 0, time.Local)), // Set the fixed CloseTime value
 		CloseCash:    utils.NullFloat64(utils.RandomFloat()),
 		CloseSummary: utils.RandomJSON(4),
 	}
@@ -36,15 +37,6 @@ func createRandomSalesTill(t *testing.T) db.SalesTill {
 	require.NoError(t, err)
 	err = json.Unmarshal(salesTill.CloseSummary, &actualCloseSummary)
 	require.NoError(t, err)
-
-	//// Round the CloseTime to the nearest millisecond
-	//expectedCloseTime := time.Unix(salesTill.CloseTime.Time.Unix(), int64(salesTill.CloseTime.Time.Nanosecond()/1000000)*1000000)
-	//
-	//// Compare only the date and time without nanosecond precision
-	//expectedCloseTime = expectedCloseTime.Round(time.Millisecond)
-	//currentTime := time.Now().Round(time.Millisecond)
-	//
-	//require.Equal(t, expectedCloseTime, currentTime, "Timestamps do not match.")
 
 	require.Equal(t, arg.TillNum, salesTill.TillNum)
 	require.Equal(t, arg.Teller, salesTill.Teller)

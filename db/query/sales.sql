b8dad0c4befbe831d6b23576bd1720eb44b7b193
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-02 12:59:28 +0300

    Sqlc Sales schema

--- db/query/sales.sql
+++ db/query/sales.sql
@@ -1,1 +1,6 @@
 -- name: CreateSales :one
+INSERT INTO sales (
+        receipt_num, till_num, product_id, item_name, vat_code, hs_code, batch_code, serial_code, serial_code_return, served_by, approved_by
+) VALUES (
+
+)

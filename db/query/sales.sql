ac879c3cdb7f8b7f0a48d86a2882c8cdff67184b
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-02 13:01:18 +0300

    Sqlc Sales schema

--- db/query/sales.sql
+++ db/query/sales.sql
@@ -3,4 +3,4 @@ INSERT INTO sales (
         receipt_num, till_num, product_id, item_name, vat_code, hs_code, batch_code, serial_code, serial_code_return, served_by, approved_by
 ) VALUES (
         $1,$2, $3, $4, $5, $6, $7, $8, $9, $10, $11
-)
+) RETURNING *;
